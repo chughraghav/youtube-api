9f614499dde906ea238b94ddb5084cf4ab31c5be
Raghav <chughraghav@gmail.com>
2019-08-10 02:29:11 +0530

    Adding iframe to VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
-export default function VideoDetail() {
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const VideoDetail = ({ video }) => {
+
+    if(!video) return <div>Loading...</div>
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return (
-        <h1>This is a Video Detail component</h1>
+        <React.Fragment>
+            <Paper elevation={6} style={{height : '70%'}}>
+                <iframe frameBorder="0" height="100%" width="100%" title="Video Player" src={videoSrc} />
+            </Paper>
+            <Paper elevation={6} style={{padding : '15px'}}>
+                <Typography variant="h4">{video.snippet.title} - {video.snippet.channelTitle}</Typography>
+                <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
+                <Typography variant="subtitle2">{video.snippet.description}</Typography>
+            </Paper>
+        </React.Fragment>
     )}
+
+export default VideoDetail;
